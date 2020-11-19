afade24a35f1bf5cfbdb4e880c4883d34ea2f606
Raum8 <shenfeldd@gmail.com>
2020-11-19 23:44:53 +0300

    MVP Weatherio done

--- app/src/main/java/com/shenfeld/weatheriomvp/app/activities/MainActivity.kt
+++ app/src/main/java/com/shenfeld/weatheriomvp/app/activities/MainActivity.kt
@@ -1,87 +1,125 @@
 package com.shenfeld.weatheriomvp.app.activities
 
 import android.annotation.SuppressLint
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.core.content.edit
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.work.*
+import androidx.work.Constraints
+import androidx.work.NetworkType
 import com.shenfeld.weatherio.data.CityDto
-import com.shenfeld.weatherio.utils.Cities
-import com.shenfeld.weatherio.utils.Cities.*
+import com.shenfeld.weatherio.utils.*
 import com.shenfeld.weatheriomvp.R
 import com.shenfeld.weatheriomvp.app.adapters.WeatherAdapter
 import com.shenfeld.weatheriomvp.app.presenters.MainActivityPresenter
 import com.shenfeld.weatheriomvp.app.views.MainActivityView
-import com.shenfeld.weatheriomvp.data.network.worker.WeatherWorker
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_main.*
 import moxy.MvpAppCompatActivity
 import moxy.presenter.InjectPresenter
 import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.math.roundToInt
+import kotlin.properties.Delegates
 
 class MainActivity : MvpAppCompatActivity(), MainActivityView {
 
     @InjectPresenter
     lateinit var mMainActivityPresenter: MainActivityPresenter
-
-    private val constraints =
-        Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+    private lateinit var disposable: Disposable
+    private lateinit var sPref: SharedPreferences
+    private lateinit var curCity: Cities
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e(MainActivity::class.java.simpleName, loadCity() + " ON CREATE")
+        setupCity()
         days_recycler_view.layoutManager = LinearLayoutManager(this)
-        setupWorker()
-        iv_refresh_weather.setOnClickListener {
-            refreshCityWeather()
+        iv_refresh_weather.setOnClickListener { refreshCityWeather() }
+        mMainActivityPresenter.onLoadWeather(curCity.lat, curCity.lon)
+        disposable = setupPeriodicRequest(curCity.lat, curCity.lon)
+    }
+
+    private fun setupCity() {
+        when (loadCity()) {
+            getString(R.string.moscow).toString() -> {
+                curCity = Cities.MOSCOW
+                //saveCity(getString(R.string.moscow))
+            }
+            getString(R.string.taganrog).toString() -> {
+                curCity = Cities.TAGANROG
+                //saveCity(getString(R.string.taganrog))
+            }
+            getString(R.string.krasnodar).toString() -> {
+                curCity = Cities.KRASNODAR
+                //saveCity(getString(R.string.krasnodar))
+            }
+            getString(R.string.rostov_on_don).toString() -> {
+                curCity = Cities.ROSTOV_ON_DON
+                //saveCity(getString(R.string.rostov_on_don))
+            }
         }
     }
 
-    private fun setupWorker(city: Cities = ROSTOV_ON_DON) {
-        val coordCity: Data =
-            Data.Builder()
-                .putDouble("lat", city.lat)
-                .putDouble("lon", city.lon).build()
-        val requestWeatherPeriodic =
-            PeriodicWorkRequestBuilder<WeatherWorker>(10, TimeUnit.SECONDS)
-                .setConstraints(constraints)
-                .addTag("WORK")
-                .setInputData(coordCity)
-                .build()
-        WorkManager.getInstance(applicationContext).enqueue(requestWeatherPeriodic)
+    private fun setupPeriodicRequest(
+        lat: Double = ROSTOV_LAT,
+        lon: Double = ROSTOV_LON
+    ): Disposable {
+        Log.e(MainActivity::class.java.simpleName, "SETUP PERIODIC REQ")
+        return Observable.interval(
+            10,
+            TimeUnit.MINUTES
+        ).observeOn(AndroidSchedulers.mainThread()).subscribe {
+            mMainActivityPresenter.onLoadWeather(lat, lon)
+            Log.d(MainActivity::class.java.simpleName, "OBSERVE ON MAIN ACTIVITY")
+        }
     }
 
-    private fun cancelWorker() {
-        WorkManager.getInstance(applicationContext).cancelAllWorkByTag("WORK")
+    fun saveCity(curCity: String) {
+        Log.e(MainActivity::class.java.simpleName, "SAVE CITY")
+        sPref = getPreferences(MODE_PRIVATE)
+        sPref.edit {
+            putString(getString(R.string.CURRENT_CITY), curCity)
+            commit()
+        }
+    }
+
+    fun loadCity(): String {
+        Log.e(MainActivity::class.java.simpleName, "LOAD CITY")
+        sPref = getPreferences(MODE_PRIVATE)
+        return sPref.getString(getString(R.string.CURRENT_CITY), getString(R.string.rostov_on_don))
+            .toString()
     }
 
     private fun refreshCityWeather() {
         when (tv_name_city.text) {
             getString(R.string.rostov_on_don) -> {
-                cancelWorker()
-                setupWorker(ROSTOV_ON_DON)
+                mMainActivityPresenter.onLoadWeather()
             }
             getString(R.string.krasnodar) -> {
-                cancelWorker()
-                setupWorker(KRASNODAR)
+                mMainActivityPresenter.onLoadWeather(KRASNODAR_LAT, KRASNODAR_LON)
             }
             getString(R.string.moscow) -> {
-                cancelWorker()
-                setupWorker(MOSCOW)
+                mMainActivityPresenter.onLoadWeather(MOSCOW_LAT, MOSCOW_LON)
             }
             getString(R.string.taganrog) -> {
-                cancelWorker()
-                setupWorker(TAGANROG)
+                mMainActivityPresenter.onLoadWeather(TAGANROG_LAT, TAGANROG_LON)
             }
         }
+        Log.e(MainActivity::class.java.simpleName, "REFRESH WEATHER")
     }
 
     @SuppressLint("SetTextI18n")
     override fun changeUI(dataWeather: CityDto) {
         days_recycler_view.adapter = WeatherAdapter(dataWeather.days, R.layout.list_item_day)
+        tv_name_city.text = loadCity()
         tv_current_main_weather.text =
             "${dataWeather.days[0].weather[0].main}, ${dataWeather.days[0].temp.day.roundToInt()}°"
         tv_current_date.text = "Humidity: ${dataWeather.days[0].humidity}%"
@@ -107,31 +145,49 @@ class MainActivity : MvpAppCompatActivity(), MainActivityView {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.moscow -> {
-                cancelWorker()
+                saveCity(getString(R.string.moscow))
+                cancelPeriodicRequest()
+                mMainActivityPresenter.onLoadWeather(MOSCOW_LAT, MOSCOW_LON)
+                disposable = setupPeriodicRequest(MOSCOW_LAT, MOSCOW_LON)
                 tv_name_city.text = getString(R.string.moscow)
-                setupWorker(MOSCOW)
             }
             R.id.rostov -> {
-                cancelWorker()
+                saveCity(getString(R.string.rostov_on_don))
+                cancelPeriodicRequest()
+                mMainActivityPresenter.onLoadWeather()
+                disposable = setupPeriodicRequest()
                 tv_name_city.text = getString(R.string.rostov_on_don)
-                setupWorker()
             }
             R.id.krasnodar -> {
-                cancelWorker()
+                saveCity(getString(R.string.krasnodar))
+                cancelPeriodicRequest()
+                mMainActivityPresenter.onLoadWeather(KRASNODAR_LAT, KRASNODAR_LON)
+                disposable = setupPeriodicRequest(KRASNODAR_LAT, KRASNODAR_LON)
                 tv_name_city.text = getString(R.string.krasnodar)
-                setupWorker(KRASNODAR)
             }
             R.id.taganrog -> {
-                cancelWorker()
+                saveCity(getString(R.string.taganrog))
+                cancelPeriodicRequest()
+                mMainActivityPresenter.onLoadWeather(TAGANROG_LAT, TAGANROG_LON)
+                disposable = setupPeriodicRequest(TAGANROG_LAT, TAGANROG_LON)
                 tv_name_city.text = getString(R.string.taganrog)
-                setupWorker(TAGANROG)
             }
         }
         return super.onOptionsItemSelected(item)
     }
 
     override fun onDestroy() {
-        cancelWorker()
+        cancelPeriodicRequest()
         super.onDestroy()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e(MainActivity::class.java.simpleName, loadCity() + " ON STOP")
+    }
+
+    private fun cancelPeriodicRequest() {
+        Log.e(MainActivity::class.java.simpleName, "DISPOSE")
+        disposable.dispose()
     }
 }
--- app/src/main/java/com/shenfeld/weatheriomvp/data/network/worker/WeatherWorker.kt
+++ app/src/main/java/com/shenfeld/weatheriomvp/data/network/worker/WeatherWorker.kt
@@ -11,13 +11,8 @@ import java.lang.Exception
 
 class WeatherWorker(appContext: Context, workerParams: WorkerParameters) :
     Worker(appContext, workerParams) {
-    private var dataPresenter: MainActivityPresenter = MainActivityPresenter()
 
     override fun doWork(): Result {
-        val lat = inputData.getDouble("lat", ROSTOV_LAT)
-        val lon = inputData.getDouble("lon", ROSTOV_LON)
-        dataPresenter.onLoadWeather(lat, lon)
-        Log.e("WORKER", "WORKER START")
         return Result.success()
     }
 }
